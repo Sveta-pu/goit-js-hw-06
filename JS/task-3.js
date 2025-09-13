854f369e4417cd93df42fad8c226d89a2c8ff2ce
Sveta <pupsadon@gmail.com>
2025-09-13 14:49:40 +0300

    task-3

--- JS/task-3.js
+++ JS/task-3.js
@@ -19,3 +19,11 @@ class StringBuilder {
     this.#value = str + this.#value + str;
   }
 }
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
